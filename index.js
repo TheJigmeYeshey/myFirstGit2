3186bc7a2e45896b54a38059208456bc8e8cd6f7
Jigme Yeshey <jigme.yeshey@griffithuni.edu.au>
2024-07-26 15:25:33 +1000

    added adding function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+console.log("Hello World");
+
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+const sum = addTwoNumbers(5, 10);
+console.log("The sum of 5 and 10 is: " + sum);
